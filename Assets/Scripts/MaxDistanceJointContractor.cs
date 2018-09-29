59f3abaf8a6832d94d560f4386993e7395bb229d
Gnaarf <gnaarf@gmx.de>
2018-09-29 20:59:41 +0200

    pull ropes

---
+++ Assets/Scripts/MaxDistanceJointContractor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxDistanceJointContractor : MonoBehaviour
+{
+    [SerializeField]
+    MaxDistanceJoint Joint;
+
+    float defaultLength;
+
+    [SerializeField]
+    float pullSpeedFactor = 1F;
+
+    // Use this for initialization
+    void Start()
+    {
+        defaultLength = Joint.MaxDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("PullRope"))
+        {
+            print("bluebb");
+            Joint.MaxDistance = 3;// -= Time.deltaTime * pullSpeedFactor;
+        }
+        else
+        {
+            Joint.MaxDistance = defaultLength;
+        }
+    }
+}
