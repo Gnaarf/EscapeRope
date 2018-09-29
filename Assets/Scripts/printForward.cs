c12daf39819b054168b5d6fc033e5ce0ae63d0b9
Leonard <leonard.schoelch@gmail.com>
2018-09-29 13:28:20 +0200

    2nd scene

---
+++ Assets/Scripts/printForward.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class printForward : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print(this.gameObject.transform.forward);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
